f6462f3cbcd31a92f1d08469683638c8e79f1887
Kxysl1k <kirilopaskivski987@gmail.com>
2025-04-12 20:49:47 +0300

    Finish setting gradle

---
+++ src/client/java/org/kxysl1k/securejoinclientmod/client/SecurejoinclientmodClient.java
@@ -0,0 +1,10 @@
+package org.kxysl1k.securejoinclientmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecurejoinclientmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/kxysl1k/securejoinclientmod/Securejoinclientmod.java
@@ -0,0 +1,10 @@
+package org.kxysl1k.securejoinclientmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Securejoinclientmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
