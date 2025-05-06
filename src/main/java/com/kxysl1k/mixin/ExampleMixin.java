5e6aa3f39bcc690c4bf5aa580e5646135fa3b775
Kxysl1k <kirilopaskivski987@gmail.com>
2025-05-06 20:45:37 +0300

    Reddit Code

--- src/main/java/com/kxysl1k/mixin/ExampleMixin.java
+++ src/main/java/com/kxysl1k/mixin/ExampleMixin.java
@@ -1,15 +1,16 @@
 package com.kxysl1k.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(MinecraftClient.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+
+	@Inject(method = "run", at = @At("HEAD"))
+	private void onRun(CallbackInfo info) {
+		System.out.println("[SecureJoinClientMod] MinecraftClient is starting!");
 	}
 }
